bb89f12dc5cdbff381dc20e991531fb2463dc562
ellag7121 <ellag7121@wrdsb.ca>
2020-09-15 13:54:39 +0000

    Finished A2Q3!

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * get 2 integers from the user, tells user the numbers in ascending order
+ * @author Graham Ellacott
  */
 public class Main {
 
@@ -9,7 +10,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //create a scanner to get user input
+    Scanner input = new Scanner(System.in);
+
+    //initialize variables
+    int integerA; //the first integer given by the user
+    int integerB; //the second integer given by the user
+
+    //ask user for first integer
+    System.out.println("Please enter an integer:");
+    //store the user's first integer in a variable
+    integerA = input.nextInt();
+
+    //ask user for second integer
+    System.out.println("Please enter another integer:");
+    //store the user's second integer in a variable
+    integerB = input.nextInt();
+
+    //check if first integer is bigger or equal to the second
+    if (integerA >= integerB){
+      //tell user integers in ascending order
+      System.out.println("Your numbers in ascending order are " + integerB + ", " + integerA);
+    }else{ //if second integer is bigger
+      //tell user integers in ascending order
+      System.out.println("Your numbers in ascending order are " + integerA + ", " + integerB);
+    }
   }
 }
